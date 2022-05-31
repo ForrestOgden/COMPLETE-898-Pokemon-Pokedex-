006338484eb198f8629586e41ce902dfc31befdb
ForrestOgden <101207689+ForrestOgden@users.noreply.github.com>
2022-05-31 11:56:49 -0500

    Update poke.js

--- poke.js
+++ poke.js
@@ -240,7 +240,7 @@ setTimeout(function () {
   }
 
   console.log(flavorText);
-}, 2500);
+}, 3000);
 
 setTimeout(function () {
   for (let i = 0; i < flavorText.length; i++) {
@@ -252,7 +252,7 @@ setTimeout(function () {
 
     div.insertAdjacentHTML("beforeend", html);
   }
-}, 6200);
+}, 4600);
 
 // function for showing stats and description on click:---------------------------
 
